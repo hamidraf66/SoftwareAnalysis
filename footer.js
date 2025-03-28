f0d740dbd7529d8cf3dcbb424a14fbe218ae1414
hamidraf <61871766+hamidraf66@users.noreply.github.com>
2025-03-28 11:52:50 +0330

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,14 @@
 // footer.js
 fetch('/footer.html')
-  .then(response => response.text())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('خطا در بارگذاری فوتر');
+    }
+    return response.text();
+  })
   .then(data => {
     document.getElementById('footer-placeholder').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('مشکل در بارگذاری فوتر:', error);
   });
